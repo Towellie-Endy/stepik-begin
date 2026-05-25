1e2d6114f0055ca33d36ef0e91b2fb3ed09d98d0
Towellie-Endy <densed24@mail.ru>
2026-05-25 16:22:15 +0300

    finish a lot of tasks

--- stepik_p1.py
+++ stepik_p1.py
@@ -1,2 +1,76 @@
 # 2.4
 
+# cost1 = int(input())
+# cost2 = int(input())
+# cost3 = int(input())
+# cost4 = int(input())
+
+# print((cost1 + cost2 + cost3 + cost4) * 3)
+
+
+# a = int(input())
+# b = int(input())
+
+# print(3 * ((a + b) * (a + b) * (a + b)) + 275 * b * b - 127 * a - 41)
+
+
+# number = int(input())
+
+# print(f'Следующее за числом {number} число: {number + 1}')
+# print(f'Для числа {number} предыдущее число: {number - 1}')
+
+
+# lenght = int(input())
+
+# print(f'Объем = {lenght * lenght * lenght}')
+# print(f'Площадь полной поверхности = {6 * (lenght * lenght)}')
+
+
+# a = int(input())
+# b = int(input())
+
+# print(f'{a} + {b} = {a + b}')
+# print(f'{a} - {b} = {a - b}')
+# print(f'{a} * {b} = {a * b}')
+
+
+# num_1 = int(input())
+# num_2 = int(input())
+# num_3 = int(input())
+# rez = (num_1 + num_2 * (num_3 - 1))
+
+# print(rez)  
+
+
+# num = int(input())
+
+# print(num, num * 2, num * 3, num * 4, num * 5, sep='---')
+
+
+# b1 = int(input())
+# q = int(input())
+# n = int(input())
+
+# print(b1 * (q ** (n - 1)))
+
+
+# students = int(input())
+# mandarin = int(input())
+
+# print(mandarin // students)
+# print(mandarin % students)
+
+
+# n = int(input())
+
+# print(n % 2 + n // 2)
+
+
+# how_minutes = int(input())
+
+# print(f'{how_minutes} мин - это {how_minutes 2// 60} час {how_minutes % 60} минут.')
+
+
+place = int(input())
+
+print((place - 1)// 4 + 1)
