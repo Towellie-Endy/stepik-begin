27560f36e52fd30fcc1926efd152477ff68b5918
Towellie-Endy <densed24@mail.ru>
2026-05-31 21:11:12 +0300

    new_day, ended 4 lesson, began 6 lesson

--- stepik_p1.py
+++ stepik_p1.py
@@ -211,3 +211,217 @@
 
 # print(min(a, b, c, d))
 
+
+
+# x = int(input())
+
+# if -30 < x <= -2 or 7 < x <= 25:
+#     print('Принадлежит')
+# else:
+#     print('Не принадлежит')
+
+
+# beauty_number = int(input())
+# numb = beauty_number // 1000
+# if 1 <= numb < 10 and (beauty_number % 7 == 0 or beauty_number % 17 == 0):
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if a + b > c and a + c > b and b + c > a:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# year = int(input())
+# print('YES' if year % 4 == 0 and not(year % 100 == 0) or year % 400 == 0 else 'NO')
+
+
+# a1 = int(input())
+# a2 = int(input())
+# a3 = int(input())
+# a4 = int(input())
+
+# if a1 == a3 or a2 == a4:
+#     print('YES')
+# else:
+#     print('NO')
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# if (-1 <= a - c <= 1) and (-1 <= b - d <= 1):
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a == b == c:
+#     print('Равносторонний')
+# elif a == b or b == c or c == a:
+#     print('Равнобедренный')
+# else:
+#     print('Разносторонний')
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if a > b > c or c > b > a:
+#     print(b)
+# if a > c > b or b > c > a:
+#     print(c)
+# if b > a > c or c > a > b:
+#     print(a)
+
+
+# 1 3 5 7 8 10 12   31
+# 4 6 9 11  30
+# 2 28
+
+# month = int(input())
+# if month in [1, 3, 5, 7, 8, 10, 12]:
+#     print(31)
+# elif month in [4, 6, 9, 11]:
+#     print(30)
+# else:
+#     print(28)
+
+
+# weight = int(input())
+
+# if weight < 60:
+#     print('Легкий вес')
+# elif weight < 64:
+#     print('Первый полусредний вес')
+# elif weight < 69:
+#     print('Полусредний вес')
+
+
+# a = int(input())
+# b = int(input())
+# operation = input()
+
+# if operation == '+':
+#     print(a + b)
+# elif operation == '-':
+#     print(a - b)
+# elif operation == '*':
+#     print(a * b)
+# elif operation == '/':
+#     if b == 0:
+#         print("На ноль делить нельзя!")
+#     else:
+#         print(a / b)
+# else:
+#     print("Неверная операция")
+
+
+# color1 = input()
+# color2 = input()
+
+# if color1 not in ("красный", "синий", "желтый") or color2 not in ("красный", "синий", "желтый"):
+#     print("ошибка цвета")
+# else:
+#     if color1 == color2:
+#         print(color1)
+#     elif (color1 == "красный" and color2 == "синий") or (color1 == "синий" and color2 == "красный"):
+#         print("фиолетовый")
+#     elif (color1 == "красный" and color2 == "желтый") or (color1 == "желтый" and color2 == "красный"):
+#         print("оранжевый")
+#     elif (color1 == "синий" and color2 == "желтый") or (color1 == "желтый" and color2 == "синий"):
+#         print("зеленый")
+
+
+# a1 = int(input())
+# b1 = int(input())
+# a2 = int(input())
+# b2 = int(input())
+
+# left = max(a1, a2)  
+# right = min(b1, b2) 
+
+# if left > right:
+#     print("пустое множество")
+# elif left == right:
+#     print(left)  
+# else:
+#     print(left, right) 
+
+
+# number = int(input())
+
+# print('YES' if number % 100 == 00 else 'NO')
+
+
+# numb1, numb2, numb3, numb4 = int(input()), int(input()), int(input()), int(input())
+
+# if (numb1 + numb2) % 2 == (numb3 + numb4) % 2:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# age = int(input())
+# sex = input()
+
+# if 10 <= age <= 15 and sex == 'f':
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+# x1 = int(input())
+# y1 = int(input())
+
+
+# x2 = int(input())
+# y2 = int(input())
+
+# if abs(x1 - x2) == abs(y1 - y2):
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# Ход слона
+
+# x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+
+# if (x1 - y1 == x2 - y2) or (x1 + y1 == x2 + y2):
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# Ход коня
+
+# x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+# difference_product = (x1 - x2) * (y1 - y2)
+
+# if difference_product == 2 or difference_product == -2:
+#     print("YES")
+# else:
+#     print("NO")
+    
+
+# Ход ферзя
+
+# x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+
+# if x1 == x2 or y1 == y2 or (y1 - y2) ** 2 == (x1 - x2) ** 2:
+#     print("YES")
+# else:
+#     print("NO")
+    
+
